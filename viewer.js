ac75402a7292a4d3a1343ee980677c07a0e5a76f
Niall Robinson <niall.h.robinson@gmail.com>
2015-02-20 11:24:26 +0000

    code the png resolutions into the name so I on't forget them

--- viewer.js
+++ viewer.js
@@ -1,7 +1,7 @@
 var renderer, sceneFirstPass, sceneSecondPass, camera, uniforms, attributes, clock, firstPassTexture, datatex;
 var meshFirstPass;
 
-var alphaCorrection = 0.04 ; // just a fudge factor
+var alphaCorrection = 0.01 ; // just a fudge factor
 var nSteps = 500;
 
 var fps = 10;
@@ -26,7 +26,7 @@ function initVis() {
 
     /***************** Data Cloud **********************/
     // load texture
-    dataTexture = THREE.ImageUtils.loadTexture('./test_blob_hr.png');
+    dataTexture = THREE.ImageUtils.loadTexture('./test_blob.png');
 
     var boxGeometry = new THREE.BoxGeometry(1.0, 1.0, 1.0); // the block to render inside
     boxGeometry.doubleSided = true;
